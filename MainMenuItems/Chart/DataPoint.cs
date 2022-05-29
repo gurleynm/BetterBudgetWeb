ba1576b11096c11a53617c11f1276218c5f00581
Nathan Gurley <gurleynm@miamioh.edu>
2022-05-28 23:16:12 -0400

    Chart Added

---
+++ MainMenuItems/Chart/DataPoint.cs
@@ -0,0 +1,23 @@
+﻿namespace BetterBudgetWeb.MainMenuItems.Chart
+{
+    public class DataPoint
+    {
+        public string Label { get; set; }
+        public double Value { get; set; }
+        public bool Negative { get; set; }
+        public DataPoint() { }
+        public DataPoint(string label, string value)
+        {
+            Label = label;
+            bool tp = double.TryParse(value, out double v);
+            if(tp)
+                Value = v;
+        }
+        
+        public DataPoint(string label, double value)
+        {
+            Label = label;
+            Value = value;
+        }
+    }
+}
