36ba022f7d77d43614be5ac0c68f9d994f9d3080
Nathan Gurley <gurleynm@miamioh.edu>
2022-08-17 12:05:13 -0400

    Added Snapshots & added Mobile FIxes

---
+++ Data/Snapshot.cs
@@ -0,0 +1,37 @@
+﻿namespace BetterBudgetWeb.Data
+{
+    public class Snapshot
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Month { get; set; }
+        public int Year { get; set; }
+        public double Person1Income { get; set; }
+        public double Person2Income { get; set; }
+        public int Person1Transactions { get; set; }
+        public int Person2Transactions { get; set; }
+        public int TotalTransactions { get; set; }
+        public double Person1AmountProjected { get; set; }
+        public double Person2AmountProjected { get; set; }
+        public double Person1AmountActual { get; set; }
+        public double Person2AmountActual { get; set; }
+        public string PassKey { get; set; } = "no";
+
+        public Snapshot() { }
+
+        public Snapshot(Snapshot other)
+        {
+            Id = other.Id;
+            Month = other.Month;
+            Year = other.Year;
+            Person1Income = other.Person1Income;
+            Person2Income = other.Person2Income;
+            Person1Transactions = other.Person1Transactions;
+            Person2Transactions = other.Person2Transactions;
+            TotalTransactions = other.TotalTransactions;
+            Person1AmountProjected = other.Person1AmountProjected;
+            Person2AmountProjected = other.Person2AmountProjected;
+            Person2AmountActual = other.Person2AmountActual;
+            Person1AmountActual = other.Person1AmountActual;
+        }
+    }
+}
---
+++ Repo/SnapshotRepo.cs
@@ -0,0 +1,92 @@
+﻿using BetterBudgetWeb.Data;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace BetterBudgetWeb.Repo
+{
+    public class SnapshotRepo
+    {
+        private static HttpClient client = new HttpClient();
+
+        private static string baseURI = Constants.BaseUri + "Snapshot";
+        public static List<Snapshot> Snapshots { get; set; } = new List<Snapshot>();
+        public static async Task<List<Snapshot>> GetSnapshotsAsync()
+        {
+            JsonSerializerOptions _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var response = await client.GetAsync(baseURI);
+            var content = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+            List<Snapshot> snaps = System.Text.Json.JsonSerializer.Deserialize<List<Snapshot>>(content, _options);
+
+            Snapshots = snaps;
+            return snaps;
+        }
+        public static List<Snapshot> GetSnapshots()
+        {
+            Task.Run(async () => await GetSnapshotsAsync());
+            return Snapshots;
+        }
+        public static async Task<List<Snapshot>> AddOrUpdateAsync(Snapshot snap)
+        {
+            HttpClient _client = new HttpClient();
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, baseURI);
+
+            snap.PassKey = Constants.PassKey;
+
+            requestMessage.Content = JsonContent.Create(snap);
+
+            var response = await _client.SendAsync(requestMessage);
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+
+            var tran = JsonConvert.DeserializeObject<Snapshot[]>(content).ToList();
+            return tran;
+        }
+        public static async Task<List<Snapshot>> RemoveAsync(Snapshot trans)
+        {
+            HttpClient _client = new HttpClient();
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Delete, baseURI);
+
+            trans.PassKey = Constants.PassKey;
+
+            requestMessage.Content = JsonContent.Create(trans);
+
+            var response = await _client.SendAsync(requestMessage);
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+
+            var tran = JsonConvert.DeserializeObject<Snapshot[]>(content).ToList();
+            return tran;
+        }public static async Task<List<Snapshot>> RemoveAsync(string id)
+        {
+            HttpClient _client = new HttpClient();
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Delete, baseURI);
+
+            var snaps = Snapshots.FirstOrDefault(t => t.Id == id);
+            snaps.PassKey = Constants.PassKey;
+
+            requestMessage.Content = JsonContent.Create(snaps);
+
+            var response = await _client.SendAsync(requestMessage);
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+
+            var tran = JsonConvert.DeserializeObject<Snapshot[]>(content).ToList();
+            return tran;
+        }
+    }
+}
