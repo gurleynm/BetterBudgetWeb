5101909dcc19b6646a92f110c1b5e7027d25c859
Nathan Gurley <44032733+gurleynm@users.noreply.github.com>
2024-09-23 08:47:18 -0400

    Init tiers commit

---
+++ Data/Tier.cs
@@ -0,0 +1,27 @@
+﻿namespace BetterBudgetWeb.Data
+{
+    public class Tier
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double MonthlyCharge { get; set; }
+        public double YearlyCharge { get; set; }
+        public string ColorScheme { get; set; }
+        public Tier() { }
+        public Tier(Tier other)
+        {
+            Id = other.Id;
+            Name = other.Name;
+            MonthlyCharge = other.MonthlyCharge;
+            YearlyCharge = other.YearlyCharge;
+            ColorScheme = other.ColorScheme;
+        }
+        public Tier(string name, double monthlyCharge, double yearlyCharge, string colorScheme)
+        {
+            Name = name;
+            MonthlyCharge = monthlyCharge;
+            YearlyCharge = yearlyCharge;
+            ColorScheme = colorScheme;
+        }
+    }
+}
