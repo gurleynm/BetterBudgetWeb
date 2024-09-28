b89968a0b4afe2e3eebee802db5f2bed0d20482a
Nathan Gurley <44032733+gurleynm@users.noreply.github.com>
2024-09-28 00:32:19 -0400

    Much prettier error message

---
+++ Data/CustomErrorBoundary.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BetterBudgetWeb.Data
+{
+    public class CustomErrorBoundary : ErrorBoundary
+    {
+        [Inject]
+        private IWebAssemblyHostEnvironment env { get; set; }
+
+        protected override Task OnErrorAsync(Exception exception)
+        {
+            if (env.IsDevelopment())
+            {
+                string c = exception.StackTrace;
+                return base.OnErrorAsync(exception);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
