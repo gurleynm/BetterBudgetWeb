eae9e8c1c1d319096d63adb216e02420478df75f
Nathan Gurley <44032733+gurleynm@users.noreply.github.com>
2024-10-25 17:56:23 -0400

    Better Colors for Donut

---
+++ Data/ColorWheel.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+
+namespace BetterBudgetWeb.Data
+{
+    public class ColorWheel
+    {
+        private static Color[] _colors = { 
+            Color.Indigo,
+            Color.Aquamarine,
+            Color.DarkCyan,
+            Color.LimeGreen,
+            Color.Goldenrod,
+            Color.Red,
+            Color.MediumVioletRed,
+            Color.Teal,
+            Color.MediumSpringGreen,
+            Color.Chartreuse,
+            Color.RoyalBlue,
+            Color.MediumSlateBlue,
+            Color.Wheat,
+            Color.Firebrick,
+            Color.CadetBlue,
+            Color.DarkOliveGreen,
+            Color.BlueViolet,
+            Color.DarkSalmon,
+            Color.Black,
+            Color.SandyBrown,
+        };
+        private static int _colorsCount => _colors.Length;
+        private static int _colorsIndex = 0;
+        public static Color NextColor()
+        {
+            _colorsIndex++;
+            if(_colorsIndex == _colors.Length)
+                _colorsIndex = 0;
+
+            return _colors[_colorsIndex];
+        }
+    }
+}
