08214a90a20f4ece7bfe13da03817d298c7044b2
Nathan Gurley <44032733+gurleynm@users.noreply.github.com>
2024-10-30 13:54:00 -0400

    Contact us stuff

---
+++ Data/EmailHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace BetterBudgetWeb.Data
+{
+    public class EmailHelper
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string Key { get; set; }
+    }
+}
---
+++ Data/EmailService.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace BetterBudgetWeb.Data
+{
+    public class EmailService
+    {
+        public static async Task SendEmailAsync(string to, string subject, string body)
+        {
+            HttpClient client = new HttpClient();
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, "https://api.gurleytech.com/Email");
+
+            requestMessage.Content = JsonContent.Create(new EmailHelper { To = to, Subject = subject, Body = body, Key = "FranklinTheDino" });
+
+            var response = await client.SendAsync(requestMessage);
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+        }
+    }
+}
