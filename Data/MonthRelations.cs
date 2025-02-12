289782d41f92c241bc5184530a6a2ead80e87e4c
Nathan Gurley <44032733+gurleynm@users.noreply.github.com>
2025-02-11 19:00:05 -0500

    Better forcecasting

--- Data/MonthRelations.cs
+++ Data/MonthRelations.cs
@@ -6,16 +6,29 @@
         public string Name { get; set; }
         public int Month { get; set; }
         public int Year { get; set; }
+        public List<ExpenseAmount> Expenses { get; set; }
         public double ProjectPerson1Income { get; set; } = int.MinValue;
         public double ProjectPerson2Income { get; set; } = int.MinValue;
         public int YearMonth => Year * 100 + Month;
         public bool Show { get; set; }
-        public MonthRelations(int monthNum, int yearNum)
+        public MonthRelations() { }
+        public MonthRelations(int monthNum, int yearNum, List<ExpenseAmount> expenses)
         {
             Name = Months[monthNum] + " " + yearNum;
             Month = monthNum;
             Year = yearNum;
             Show = false;
+            Expenses = expenses;
+        }
+        public MonthRelations(MonthRelations other)
+        {
+            Name = other.Name;
+            Month = other.Month;
+            Year = other.Year;
+            Show = other.Show;
+            ProjectPerson1Income = other.ProjectPerson1Income;
+            ProjectPerson2Income = other.ProjectPerson2Income;
+            Expenses = other.Expenses;
         }
     }
 }
